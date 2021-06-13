27449944cf6ae54beca4e5dd9646cea9db8513dc
Rosch <jaketr00@gmail.com>
2021-06-12 21:16:19 -0500

    Update index.d.ts

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,3 @@
-declare function cleverbot(stimlus: string, context?: string[], language?: string): Promise<string>;
+type sendMessage = (stimlus: string, context?: string[], language?: string) => Promise<String>;
+declare function cleverbot(): Promise<sendMessage>;
 export default cleverbot;
